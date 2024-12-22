43271ae91a53a3ec5758b277ff084aea8ac58633
Anders Pitman <&ers@apitman.com>
2024-12-22 13:33:37 -0700

    Rename decent_auth.wasm to decentauth.wasm

--- wasm.js
+++ wasm.js
@@ -15,7 +15,7 @@ function decode(valueBytes) {
   return JSON.parse(decoder.decode(valueBytes));
 }
 
-const wasmBytes = await readFile("./decent_auth.wasm");
+const wasmBytes = await readFile("./decentauth.wasm");
 const module = await WebAssembly.compile(wasmBytes); 
 
 async function createWasmPlugin(config, kvStore) {
